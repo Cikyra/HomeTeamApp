04a640a2d537839e3e87e2c87547ebe3189d16a1
Laura Riley <laura.riley@sjsu.edu>
2025-01-28 13:34:56 -0800

    initial setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,98 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.ksp)
+
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.hilt)
+}
+android {
+    namespace = "com.cikyra.hometeam"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.cikyra.hometeam"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    // Glide
+    implementation(libs.glide)
+    ksp(libs.glide.compiler)
+
+    // Retrofit
+    implementation(libs.retrofit)
+
+    // Moshi
+    implementation(libs.moshi)
+    implementation(libs.moshi.kotlin)
+
+    // Moshi converter for Retrofit
+    implementation(libs.retrofit.moshi.converter)
+
+    // RecyclerView
+    implementation(libs.androidx.recyclerview)
+
+    // Compose
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+
+    // Coroutine scope
+    implementation(libs.androidx.lifecycle.runtime)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.lifecycle.livedata)
+
+    // Material Design
+    implementation(libs.avs.material)
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.material3)
+
+    // Core
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodelcompose)
+    implementation(libs.kotlinx.serialization.json)
+
+
+    // Testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+    androidTestImplementation(libs.androidx.test.compose.ui.junit)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+}
